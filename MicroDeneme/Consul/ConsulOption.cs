e6f393e327cb4877c54beeb1dcf40ad674419cb7
Zehra Karahan <zehrakarahan96@gmail.com>
2022-01-06 15:39:25 +0300

    ocelot ve consul apigateway ile ornek projesi

---
+++ MicroDeneme/Consul/ConsulOption.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bar.Consul
+{
+    public class ConsulOption
+    {
+        public string ServiceName { get; set; }
+
+        /// <summary>
+        /// 服务IP
+        /// </summary>
+        public string ServiceIP { get; set; }
+
+        /// <summary>
+        /// 服务端口
+        /// </summary>
+        public int ServicePort { get; set; }
+
+        /// <summary>
+        /// 服务健康检查地址
+        /// </summary>
+        public string ServiceHealthCheck { get; set; }
+
+        /// <summary>
+        /// Consul 地址
+        /// </summary>
+        public string Address { get; set; }
+    }
+}
